0539891b947056578e6febebab69582e19c42c8e
matttiondeangelo25-eng <mattiondeangelo25@gmail.com>
2026-03-29 18:45:47 -0400

    Added script.js and api key

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+const apiKey ="Giphy Test App"
+const url = https://api.giphy.com/v1/stickers/search?api_key=&q=frogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips
